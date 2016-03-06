eafbc6035d6d82a68b352bc333783d4fe19437e5
James Laurelli <j.m.laurelli@gmail.com>
2016-03-05 20:47:15 -0500

    Add porfolio chart placeholder

--- assets/js/portfoliodetails.jsx
+++ assets/js/portfoliodetails.jsx
@@ -3,12 +3,27 @@
 var React = require("react");
 
 
+var PortfolioChart = React.createClass({
+    render: function() {
+        return (
+            <img height="500px"
+                 width="500px"
+                 style={{border: "black solid 1px"}}
+                 alt="PLACEHOLDER CHART">
+            </img>
+        );
+    }
+});
+
+
 var PortfolioDetails = React.createClass({
     render: function() {
         return (
             <section className="col-lg-8">
                 <h1>Your Porfolio Performance:</h1>
-                <div></div>
+                <div>
+                    <PortfolioChart />
+                </div>
             </section>
         );
     }
