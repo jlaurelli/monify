ee3d324048b542a892c20ac20f92611f66079892
James Laurelli <j.m.laurelli@gmail.com>
2016-03-13 15:17:38 -0400

    Add StockPrice model

--- market_analyzer/models.py
+++ market_analyzer/models.py
@@ -3,14 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Investment(models.Model):
+class Stock(models.Model):
     # All U.S. money market fund ticker symbols are five letters long.
     symbol = models.CharField(unique=True, max_length=5)
     name = models.CharField(max_length=200)
-    current_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __unicode__(self):
         return self.name
+
+
+class StockPrice(models.Model):
+    ts = models.DateTimeField(db_index=True)
+    stock = models.ForeignKey(Stock, related_name="prices")
+    price = models.DecimalField(max_digits=20, decimal_places=6)
+
+
+class Investment(models.Model):
+    stock = models.ForeignKey(Stock)
+    shares = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return str(self.stock, self.shares)
 
 
 class Portfolio(models.Model):
